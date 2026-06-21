59d567c86b5caef070bea3655873ebadb2d5bedf
hong-phuc2907 <vipem1763@gmail.com>
2026-06-21 23:57:40 +0700

    quiz.tsx

--- artifacts/vocab-app/src/pages/quiz.tsx
+++ artifacts/vocab-app/src/pages/quiz.tsx
@@ -75,7 +75,9 @@ sec: number
 const m = Math.floor(sec / 60);
 const s = sec % 60;
 
-return "${m .toString() .padStart(2, "0")}:${s .toString() .padStart(2, "0")}";
+return `${m.toString().padStart(2, "0")}:${s
+  .toString()
+  .padStart(2, "0")}`;
 }
 
 export default function Quiz() {
